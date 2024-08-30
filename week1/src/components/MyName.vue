79d9efd727181be3ef63dfe393de8bf773987be0
rpan0029 <rpan0029@student.monash.edu>
2024-08-30 20:52:38 +1000

    week1

---
+++ week1/src/components/MyName.vue
@@ -0,0 +1,35 @@
+<template>
+    <div>
+      <p>Hello, my name is RuiyingPan!</p>
+    </div>
+  </template>
+  
+  <style scoped>
+.my-name {
+  text-align: center;  /* Center the text horizontally */
+}
+
+h1 {
+  font-weight: 500;
+  font-size: 2.6rem;
+  position: relative;
+  top: -10px;
+}
+
+h3 {
+  font-size: 1.2rem;
+}
+
+.greetings h1,
+.greetings h3 {
+  text-align: center;
+}
+
+@media (min-width: 1024px) {
+  .greetings h1,
+  .greetings h3 {
+    text-align: left;
+  }
+}
+  </style>
+  
