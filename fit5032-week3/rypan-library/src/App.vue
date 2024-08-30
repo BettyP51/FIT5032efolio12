be31508450f01d01fa9a2e43440b14e52c1c0396
rpan0029 <rpan0029@student.monash.edu>
2024-08-30 20:56:25 +1000

    week3

---
+++ fit5032-week3/rypan-library/src/App.vue
@@ -0,0 +1,16 @@
+<script setup>
+import HelloWorld from './components/HelloWorld.vue'
+import TheWelcome from './components/TheWelcome.vue'
+import JSON from './components/JSON.vue';
+//import JSONSolution from './components/JSONSolution.vue';
+import RYForm from './components/RYForm.vue';
+</script>
+
+<template>
+  <!-- <JSON /> -->
+  <!-- <JSONSolution /> -->
+  <RYForm />
+</template>
+
+<style>
+</style>
