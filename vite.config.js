49b69f958f44c8a1b4e2a4175310c57dade11251
rpan0029 <rpan0029@student.monash.edu>
2024-10-18 01:59:47 +1100

    Initial commit

--- vite.config.js
+++ vite.config.js
@@ -11,5 +11,9 @@ export default defineConfig({
     alias: {
       '@': fileURLToPath(new URL('./src', import.meta.url))
     }
-  }
+  },
+  // 设置基础路径
+  base: process.env.NODE_ENV === 'production'
+    ? '/<repository-name>/'  // 将 <repository-name> 替换为您的 GitHub 仓库名
+    : '/'
 })
